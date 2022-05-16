a6ed1737845fe740a3d66c8394065402d159dca4
Itamar Podolsky <itamarpo@mta.ac.il>
2022-05-16 13:01:47 +0300

    TABLES

---
+++ Alternative banking system/JAVAFX/src/userinterface/table/loantable/FinishedLoanTableController.java
@@ -0,0 +1,24 @@
+package userinterface.table.loantable;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class FinishedLoanTableController {
+
+    //JavaFX components
+    @FXML
+    private TableView<?> finishedTable;
+    @FXML private TableColumn<?, ?> loanID;
+    @FXML private TableColumn<?, ?> category;
+    @FXML private TableColumn<?, ?> owner;
+    @FXML private TableColumn<?, ?> amount;
+    @FXML private TableColumn<?, ?> duration;
+    @FXML private TableColumn<?, ?> interest;
+    @FXML private TableColumn<?, ?> timePerPayment;
+    @FXML private TableColumn<?, ?> listOfLenders;
+    @FXML private TableColumn<?, ?> payments;
+    @FXML private TableColumn<?, ?> startingTime;
+    @FXML private TableColumn<?, ?> finishingTime;
+
+}

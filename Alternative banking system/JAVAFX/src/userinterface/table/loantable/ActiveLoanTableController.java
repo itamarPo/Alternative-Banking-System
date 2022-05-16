8e3cba8d1411e39e966b3bbe16fc1d01e46921af
Itamar Podolsky <itamarpo@mta.ac.il>
2022-05-17 01:31:23 +0300

    TABLES

--- Alternative banking system/JAVAFX/src/userinterface/table/loantable/ActiveLoanTableController.java
+++ Alternative banking system/JAVAFX/src/userinterface/table/loantable/ActiveLoanTableController.java
@@ -1,11 +1,17 @@
 package userinterface.table.loantable;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class ActiveLoanTableController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ActiveLoanTableController implements Initializable {
 
     //JavaFX components
+    @FXML private TableView<?> tableView;
     @FXML private TableColumn<?, ?> loanID;
     @FXML private TableColumn<?, ?> category;
     @FXML private TableColumn<?, ?> owner;
@@ -21,4 +27,9 @@ public class ActiveLoanTableController {
     @FXML private TableColumn<?, ?> allInterestPayedSoFar;
     @FXML private TableColumn<?, ?> allInitialLeftToPay;
     @FXML private TableColumn<?, ?> allInterestLeftToPay;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
 }
--- Alternative banking system/JAVAFX/src/userinterface/table/loantable/NewLoanTableController.java
+++ Alternative banking system/JAVAFX/src/userinterface/table/loantable/NewLoanTableController.java
@@ -1,15 +1,26 @@
 package userinterface.table.loantable;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class NewLoanTableController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NewLoanTableController implements Initializable {
 
     //JavaFX components
+    @FXML private TableView<?> tableView;
     @FXML private TableColumn<?, ?> loanID;
     @FXML private TableColumn<?, ?> category;
     @FXML private TableColumn<?, ?> owner;
     @FXML private TableColumn<?, ?> duration;
     @FXML private TableColumn<?, ?> interest;
     @FXML private TableColumn<?, ?> timePerPayment;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
 }

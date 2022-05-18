97d3dca16c40aa641c29b5c1a403c8456428add5
itay0 <itay03065@gmail.com>
2022-05-18 17:19:02 +0300

    Changes by itamarpo

--- Alternative banking system/JAVAFX/src/userinterface/customer/information/InformationTabController.java
+++ Alternative banking system/JAVAFX/src/userinterface/customer/information/InformationTabController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import userinterface.table.loantable.*;
 
@@ -11,43 +12,46 @@ import userinterface.table.loantable.*;
 public class InformationTabController {
 
     //Sub components
-    @FXML ScrollPane newLoanerTable;
-    @FXML NewLoanTableController newLoanerTableController;
-    @FXML ScrollPane pendingLoanerTable;
-    @FXML PendingLoanTableController pendingLoanerTableController;
-    @FXML ScrollPane activeLoanerTable;
-    @FXML ActiveLoanTableController activeLoanerTableController;
-    @FXML ScrollPane riskLoanerTable;
-    @FXML RiskLoanTableController riskLoanerTableController;
-    @FXML ScrollPane finishedLoaderTable;
-    @FXML FinishedLoanTableController finishedLoaderTableController;
+    @FXML private ScrollPane newLoanerTable;
+    @FXML private NewLoanTableController newLoanerTableController;
+    @FXML private ScrollPane pendingLoanerTable;
+    @FXML private PendingLoanTableController pendingLoanerTableController;
+    @FXML private ScrollPane activeLoanerTable;
+    @FXML private ActiveLoanTableController activeLoanerTableController;
+    @FXML private ScrollPane riskLoanerTable;
+    @FXML private RiskLoanTableController riskLoanerTableController;
+    @FXML private ScrollPane finishedLoaderTable;
+    @FXML private FinishedLoanTableController finishedLoaderTableController;
 
-    @FXML ScrollPane newLenderTable;
-    @FXML NewLoanTableController newLenderTableController;
-    @FXML ScrollPane pendingLenderTable;
-    @FXML PendingLoanTableController pendingLenderTableController;
-    @FXML ScrollPane activeLenderTable;
-    @FXML ActiveLoanTableController activeLenderTableController;
-    @FXML ScrollPane riskLenderTable;
-    @FXML RiskLoanTableController riskLenderTableController;
-    @FXML ScrollPane finishedLenderTable;
-    @FXML FinishedLoanTableController finishedLenderTableController;
+    @FXML private ScrollPane newLenderTable;
+    @FXML private NewLoanTableController newLenderTableController;
+    @FXML private ScrollPane pendingLenderTable;
+    @FXML private PendingLoanTableController pendingLenderTableController;
+    @FXML private ScrollPane activeLenderTable;
+    @FXML private ActiveLoanTableController activeLenderTableController;
+    @FXML private ScrollPane riskLenderTable;
+    @FXML private RiskLoanTableController riskLenderTableController;
+    @FXML private ScrollPane finishedLenderTable;
+    @FXML private FinishedLoanTableController finishedLenderTableController;
+
+    @FXML private AnchorPane transactionInfo;
+    @FXML private AccountTransactionController transactionController;
 
     //JavaFX components
-    @FXML ScrollPane informationSP;
-    @FXML BorderPane informationBP;
-    @FXML TabPane loanerTB;
-    @FXML Tab newLoanerTab;
-    @FXML Tab pendingLoanerTab;
-    @FXML Tab activeLoanerTab;
-    @FXML Tab riskLoanerTab;
-    @FXML Tab finishedLoanerTab;
-    @FXML TabPane lenderTB;
-    @FXML Tab newLenderTab;
-    @FXML Tab pendingLenderTab;
-    @FXML Tab activeLenderTab;
-    @FXML Tab riskLenderTab;
-    @FXML Tab finishedLenderTab;
+    @FXML private ScrollPane informationSP;
+    @FXML private BorderPane informationBP;
+    @FXML private TabPane loanerTB;
+    @FXML private Tab newLoanerTab;
+    @FXML private Tab pendingLoanerTab;
+    @FXML private Tab activeLoanerTab;
+    @FXML private Tab riskLoanerTab;
+    @FXML private Tab finishedLoanerTab;
+    @FXML private TabPane lenderTB;
+    @FXML private Tab newLenderTab;
+    @FXML private Tab pendingLenderTab;
+    @FXML private Tab activeLenderTab;
+    @FXML private Tab riskLenderTab;
+    @FXML private Tab finishedLenderTab;
 
 
 }

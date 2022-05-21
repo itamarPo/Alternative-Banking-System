fa4843370a3db0cef1ab3ca59fc8e6f51909ac06
itay0 <itay03065@gmail.com>
2022-05-21 16:38:11 +0300

    file errors added

--- Alternative banking system/JAVAFX/src/userinterface/MainController/MainController.java
+++ Alternative banking system/JAVAFX/src/userinterface/MainController/MainController.java
@@ -43,8 +43,8 @@ public class MainController {
 
     //****Regular Methods****//
     public void changeScene(String newChoice){
-        double width = primaryStage.getWidth();
-        double height = primaryStage.getHeight();
+//        double width = primaryStage.getWidth();
+//        double height = primaryStage.getHeight();
         if(newChoice.equals("Admin")){
             topAdminController.setTopBar(topCustomerController,newChoice);
             primaryStage.setScene(AdminScene);
@@ -54,8 +54,8 @@ public class MainController {
             primaryStage.setScene(CustomerScene);
         }
 
-        primaryStage.setWidth(width);
-        primaryStage.setHeight(height);
+//        primaryStage.setWidth(width);
+//        primaryStage.setHeight(height);
 
 
     }

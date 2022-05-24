32839786f102950f008d7ae7e517e7991fcaecdc
Itamar Podolsky <itamarpo@mta.ac.il>
2022-05-24 17:45:33 +0300

    lenders list now poping up, but failing to load information

--- Alternative banking system/JAVAFX/src/userinterface/table/LendersTableController.java
+++ Alternative banking system/JAVAFX/src/userinterface/table/LendersTableController.java
@@ -24,11 +24,15 @@ public class LendersTableController {
 
     private Stage popUpLenderStage;
 
+    public Stage getPopUpLenderStage() {
+        return popUpLenderStage;
+    }
+
     private Scene popUpLenderScene;
 
     public void initialize(){
-        loanerName.setCellValueFactory(new PropertyValueFactory<Map<String, Double> ,String>("name"));
-        amount.setCellValueFactory(new PropertyValueFactory<Map<String, Double>, Double>("amount"));
+        loanerName.setCellValueFactory(new PropertyValueFactory<Map<String, Double> ,String>(""));
+        amount.setCellValueFactory(new PropertyValueFactory<Map<String, Double>, Double>(""));
     }
 
     public void setValues(Map<String, Double> values){

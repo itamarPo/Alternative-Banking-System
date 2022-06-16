f93d4633340b20f1a29885376f1ea5fcdfe0394c
Itamar Podolsky <itamarpo@mta.ac.il>
2022-06-16 17:29:49 +0300

    Changes by itay0

--- Alternative banking system/CustomerApp/src/customercomponents/customerlogin/CustomerLoginController.java
+++ Alternative banking system/CustomerApp/src/customercomponents/customerlogin/CustomerLoginController.java
@@ -1,11 +1,23 @@
 package customercomponents.customerlogin;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.Response;
+import org.controlsfx.control.Notifications;
+import userinterface.Constants;
+import userinterface.HttpAdminUtil;
+
+import java.io.IOException;
 
 public class CustomerLoginController {
 
@@ -25,7 +37,36 @@ public class CustomerLoginController {
     //Regular Methods
     @FXML
     void loginOnAction(ActionEvent event) {
-        primaryStage.setScene(customerScreenScene);
+        String userName = nameTextField.getText();
+        if(userName.equals("")){
+            //error label turn on
+            return;
+        }
+
+        String finalUrl = HttpUrl.parse(Constants.FULL_PATH_DOMAIN + Constants.LOGIN_RESOURCE).newBuilder()
+                .addQueryParameter("userName", userName).addQueryParameter("isAdmin", "false")
+                .build()
+                .toString();
+
+        HttpAdminUtil.runAsync(finalUrl, false ,new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Platform.runLater(() ->
+                        Notifications.create().title("Error").text("BLA BLA!").hideAfter(Duration.seconds(5)).position(Pos.CENTER).show());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.code() != 200) {
+                    Platform.runLater(() ->
+                            Notifications.create().title("Error").text("This user name is already in use!").hideAfter(Duration.seconds(5)).position(Pos.CENTER).show());
+                } else {
+                    Platform.runLater(() ->
+                            primaryStage.setScene(customerScreenScene));
+                }
+            }
+        });
+
     }
 
 }

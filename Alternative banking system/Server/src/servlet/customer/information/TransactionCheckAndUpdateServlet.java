5bbe59ecacf76953488cacae6ec180b066d64627
itay0 <itay03065@gmail.com>
2022-07-06 19:02:03 +0300

    project working, except for servlet changes for postman and chat bonus

--- Alternative banking system/Server/src/servlet/customer/information/TransactionCheckAndUpdateServlet.java
+++ Alternative banking system/Server/src/servlet/customer/information/TransactionCheckAndUpdateServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import utils.EngineServlet;
+import utils.ServerChecks;
 
 import java.io.IOException;
 
@@ -17,24 +18,57 @@ import static userinterface.Constants.*;
 public class TransactionCheckAndUpdateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = ServerChecks.getUserName(request);
+        //Session doesn't exist!
+        if (userName == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ServerChecks.setMessageOnResponse(response.getWriter(), ServerChecks.NO_SESSION_FOUND);
+            return;
+        }
         Engine engine = EngineServlet.getEngine(getServletContext());
-        Boolean chargeOrWithdraw = Boolean.parseBoolean(request.getParameter("chargeOrWithdraw"));
-        Double amount = Double.parseDouble(request.getParameter(AMOUNT));
-        String userName = String.valueOf(request.getSession().getAttribute(USERNAME));
-        Gson gson = GSON_INSTANCE;
-        if(chargeOrWithdraw)
-        {
-           engine.addMoneyToAccount(engine.getCustomerByName(userName), amount);
-           response.getWriter().println(gson.toJson(engine.getCustomerInfo(userName)));
+        //User isn't customer!
+        if (engine.isUserAdmin(userName)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            ServerChecks.setMessageOnResponse(response.getWriter(), ServerChecks.LIMITED_ACCESS);
+            return;
         }
-        else{
-            try {
-                engine.drawMoneyFromAccount(engine.getCustomerByName(userName), amount);
-                response.getWriter().println(gson.toJson(engine.getCustomerInfo(userName)));
-            } catch (Exception e) {
-                response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
-                response.getWriter().println(e.toString());
+        //Server is in rewind!
+        if (engine.getServerStatus().equals(REWIND)) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            ServerChecks.setMessageOnResponse(response.getWriter(), ServerChecks.STATUS_PROBLEM);
+            return;
+        }
+        try{
+            String chargeorWithdraw = request.getParameter("charge");
+            if(!chargeorWithdraw.equalsIgnoreCase("true") && !chargeorWithdraw.equalsIgnoreCase("false")){
+                throw new Exception();
             }
+            Boolean chargeOrWithdraw = Boolean.parseBoolean(request.getParameter("charge"));
+            Double amount = Double.parseDouble(request.getParameter(AMOUNT));
+            if(amount <= 0){
+                throw new Exception();
+            }
+            Gson gson = GSON_INSTANCE;
+            if(chargeOrWithdraw)
+            {
+                engine.addMoneyToAccount(engine.getCustomerByName(userName), amount);
+                response.setContentType("application/json");
+                response.getWriter().println(gson.toJson(engine.getCustomerInfo(userName)));
+            } else{
+                try {
+                    engine.drawMoneyFromAccount(engine.getCustomerByName(userName), amount);
+                    response.setContentType("application/json");
+                    ServerChecks.setMessageOnResponse(response.getWriter(),gson.toJson(engine.getCustomerInfo(userName)));
+//                    response.getWriter().println(gson.toJson(engine.getCustomerInfo(userName)));
+                } catch (Exception e) {
+                    response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+                    ServerChecks.setMessageOnResponse(response.getWriter(), e.toString());
+//                    response.getWriter().println(e.toString());
+                }
+            }
+        } catch (Exception e){
+            ServerChecks.setMessageOnResponse(response.getWriter(), "One of the query parameters is missing or invalid!");
         }
+
     }
 }
